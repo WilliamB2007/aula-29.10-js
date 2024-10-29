ebdab478bc05d3fcfb959e6949d88b9ed2677795
WilliamB2007 <w.breno1008@gmail.com>
2024-10-29 14:22:18 -0300

    Add files via upload

---
+++ aulas front-end/atividades.js
@@ -0,0 +1,50 @@
+//let resultado1 = 3 + 4
+//console.log(resultado1)
+
+//let resultado = (3 * 5) / 2;
+//console.log(resultado);
+
+//resultado1 = 4 - 5 * -1
+//console.log(resultado1)
+
+//resultado1 = 234 % 5
+//console.log(resultado1)
+
+//let primeiroNumero = 5 
+//let segundonumero = 4
+//let resultado = primeiroNumero !== segundoNumero
+//console.log(resultado)
+
+//let numero1 = 3;
+//let numero2 = 5;
+//let resultado = numero1 !== numero2;
+//console.log(resultado);
+
+//let numero1 = 3;
+//let numero2 = 5;
+
+//let resultado = numero1 > numero2;
+//console.log(resultado);
+
+//let numero = 3;
+//let numero2 = 5;
+
+//let resultado = numero1 < numero2;
+//console.log(resultado);
+
+let A = true
+let B = false
+let C = true
+
+let operacao1 = A||B
+console.log(operacao1)
+
+
+let operacao2 = B||C
+console.log(operacao2)
+
+let operacao3 = A||C
+console.log(operacao3)
+
+letoperacao4 = A||B||C
+console.log(operacao4)
---
+++ aulas front-end/funcoes.js
@@ -0,0 +1,21 @@
+//function imprimirolamundo(){
+//    console.log("Olá mundo!");
+//}
+//imprimirolamundo()
+
+///function nome (nome){
+//console.log(ola $ {nome })
+  //  console.log("ola $(nome) $(sobrenome)")
+//}
+//nome(Neemias)
+//nome("Neymar njr" )
+//nome("cassio  Alves")
+//nome("Erick silva")
+
+function somar(numero1 , numero2){
+const resultado = numero1 + numero2
+return resultado 
+}
+const somadosnumeros = somar(6 , 7)
+
+console.log(somar(6 , 7))
